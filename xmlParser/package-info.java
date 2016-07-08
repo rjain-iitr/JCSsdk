47ee075e9b18870faefbc2a0c7b226255a18510e
Shubham Jain <shubham2911@users.noreply.github.com>
2016-07-08 19:11:03 +0530

    Create package-info.java

---
+++ xmlParser/package-info.java
@@ -0,0 +1,4 @@
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://compute.jiocloudservices.com/doc/2016-03-01/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package xmlParser;
+
