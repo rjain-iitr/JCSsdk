4792ec24e6d18f621862a6135b2a94659c676924
Shubham Jain <shubham2911@users.noreply.github.com>
2016-07-11 12:28:03 +0530

    Add files via upload

--- xmlParser/package-info.java
+++ xmlParser/package-info.java
@@ -1,8 +1,4 @@
-/**
- * 
- */
-/**
- * @author shubham
- *
- */
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://compute.jiocloudservices.com/doc/2016-03-01/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package xmlParser;
+
